edddc07d9b3232ac9961aac3bbb556184b308759
brightestsirius <katherina.shypovskaya@gmail.com>
2021-03-21 12:52:10 +0200

    feature/task-3 – Ivanka Hero

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ export const heroesWrapper = document.querySelector('#heroes__wrapper');
 
 import {Pokemon} from './task-1.js';
 import {Zeus} from './task-2.js';
+import {Ivanka} from './task-3.js';
 
 console.log(Pokemon);
 console.log(Zeus);
+console.log(Ivanka);
