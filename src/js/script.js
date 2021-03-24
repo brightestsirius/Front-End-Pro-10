c9d5237012130fe74566d0a5a0c7bb479ec90bf9
yanareva <esmira2008@gmail.com>
2021-03-25 00:55:02 +0200

    feature/task-8 - create hero Panda

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,25 @@
 export const heroesWrapper = document.querySelector('#heroes__wrapper');
 
-import {Pokemon} from './task-1.js';
-import {Zeus} from './task-2.js';
-import {Ivanka} from './task-3.js';
-import {Batman} from './task-13.js';
-import {Goose} from './task-7.js';
-import {Cyborg} from './task-6.js';
+import { Pokemon } from './task-1.js';
+import { Zeus } from './task-2.js';
+import { Ivanka } from './task-3.js';
+import { Batman } from './task-13.js';
+import { Goose } from './task-7.js';
+import { Cyborg } from './task-6.js';
+import { Panda } from './task-8.js';
 
 // import * as exampleData from './exampleModule.js';
 // import {userName} from './exampleModule.js';
 // import {x, userName} from './exampleModule.js';
 
 import * as exampleModuleData from './exampleModule.js';
-import {helloUser} from './exampleModule.js';
+import { helloUser } from './exampleModule.js';
 
 console.log(Pokemon);
 console.log(Zeus);
 console.log(Ivanka);
 console.log(Batman);
-console.log(Goose)
+console.log(Goose);
 
 Ivanka.renderInfo();
 
