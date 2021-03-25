b451c46c1c144a3b1934603b5bee23b163b0e5db
OlenaPustova <i.am.elena.p@gmail.com>
2021-03-25 18:27:23 +0200

    feature/task-5 finished create messi

--- src/js/task-5.js
+++ src/js/task-5.js
@@ -1,11 +1,11 @@
-import { heroesWrapper } from "./script.js";
+import {heroesWrapper} from './script.js';
 
 class Hero {
     constructor() {
         this.getInfo();
     }
     async getInfo() {
-        let response = await fetch("./json/task-5.json"),
+        let response = await fetch('./json/task-5.json'),
             data = await response.json();
         this.setInfo(data);
     }
@@ -17,20 +17,49 @@ class Hero {
         this.renderInfo();
     }
     renderInfo() {
-        console.log(this);
-        let table = document.createElement("table");
+        let table = document.createElement('table');
         table.id = 'task-5';
+
+        let tr = document.createElement('tr');
+
         let tds = [];
 
         for (let key in this) {
-            let td = `<td>${this[key]}</td>`;
+            let content = Array.isArray(this[key])
+                ? this[key].join(', ')
+                : this[key];
+
+            if (key === 'image') {
+                content = this.renderImg();
+            }
+            let td = `<td>${content}</td>`;
             tds.push(td);
         }
 
-        table.innerHTML = `<tr>
-            ${tds.join("")}
-        </tr>`;
+        tr.innerHTML = tds.join('');
+        tr.append(this.renderBtn());
+        table.append(tr);
+
         heroesWrapper.append(table);
+    }
+
+    renderImg() {
+        return `<img src="./images/${this.image}" alt="${this.name}" height="100">`;
+    }
+
+    renderBtn() {
+        let td = document.createElement('td');
+        let btn = document.createElement('button');
+        btn.innerHTML = `Click me`;
+        btn.addEventListener('click', this.btnEvent.bind(this));
+
+        td.append(btn);
+
+        return td;
+    }
+    btnEvent() {
+        let addTd =  document.querySelector('#task-5 td img')
+        addTd.classList.toggle('add__rotate');
     }
 }
 
